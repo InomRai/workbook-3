b90882ea9336e88beca0d8fc8f21b785306edd86
InomRai <monirai849@gmail.com>
2025-04-23 16:10:39 -0400

    learning

--- PayrollCalculator/src/main/java/com/pluralsight/Calculator.java
+++ PayrollCalculator/src/main/java/com/pluralsight/Calculator.java
@@ -1,21 +1,27 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.NumberFormat;
+import java.io.*;
+import java.util.Scanner;
 
 
 public class Calculator {
     public static void main(String[] args) {
         String fileName = "src/main/resources/employees.csv";
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter the name of your employee file: ");
+        String payrollFile = scanner.nextLine();
+        System.out.print("Enter the name of the file you want to create: ");
+        String saveFileName = scanner.nextLine();
+
 
 
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader = new BufferedReader(new FileReader("src/main/resources/" + payrollFile));
             String line;
-
+            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/" + saveFileName));
+            reader.readLine();
             while ((line = reader.readLine()) != null) {
                 if (line.trim().isEmpty()) continue;
 
@@ -25,26 +31,33 @@ public class Calculator {
                 if (input.length < 4) {
                     System.out.println("Skipping invalid line: " + line);
                     continue;
+                }
 
-
-                    int id = Integer.parseInt(input[0].trim());
+                int id = Integer.parseInt(input[0].trim());
                     String name = input[1].trim();
                     Double hoursWorked = Double.parseDouble(input[2].trim());
                     Double payRate = Double.parseDouble(input[3].trim());
 
 
                     Employee emp = new Employee(id, name, hoursWorked, payRate);
+                    double grossPay = emp.getGrossPay();
                     System.out.printf("ID: %d | Name: %s | Gross Pay: $%.2f%n", emp.getEmployeeID(), emp.getName(), emp.getGrossPay());
 
 
+
+                writer.write(String.format("%d|%s|%.2f", id, name, grossPay));
+                writer.newLine();
+
+
+
                 }
-            }
+
 
 
         reader.close();
+        writer.close();
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
